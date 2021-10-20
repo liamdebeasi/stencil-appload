1e68d8e1b0b367d8842bfe18a6bf997225e50e3d
Liam DeBeasi <liamdebeasi@icloud.com>
2021-10-20 10:48:03 -0400

    Add repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,19 @@ export class AppComponent {
       console.log(e);
     });
   }
+  
+  public cloneElement(insideIonApp = false) {
+    const button = document.createElement('ion-button');
+    
+    if (insideIonApp) {
+      const app = document.querySelector('ion-app');
+      button.innerText = 'Cloned in App';
+
+      app.appendChild(button);
+    } else {
+      button.innerText = 'Cloned Outside App';
+
+      document.body.appendChild(button);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
